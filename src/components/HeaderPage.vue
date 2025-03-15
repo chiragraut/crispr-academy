9989c995e874a273c6f47b90a8744218df6cd7ce
chiragraut <chiragrautmail@gmail.com>
2025-03-15 18:12:05 +0530

    Your commit message here

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- Header -->
   <div class="hero-title container mt-2" style="text-align: justify;">
-    CRISPER Academy: Merge to Emerge
+    CRISPER Academy: <span>Merge to Emerge</span>
   </div>
 </template>
 <style scoped></style>
